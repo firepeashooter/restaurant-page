2c9910bf90d92a13caf487ea061b1cdb7197a30c
Ben Braun <benjamin.braun2003@gmail.com>
2025-08-07 12:32:31 -0400

    Add code to handle tab switching

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import { generateHome } from './generateHome';
 import { generateMenu } from './generateMenu';
 import { generateContact } from './generatingContact';
 
-//we need event delegation using tha nav as the base
 
-const navBar = document.querySelector("nav--bar");
+const navBar = document.querySelector(".nav--bar");
+const content = document.querySelector("#content");
+const aboutButton = document.querySelector("#about");
+const menuButton = document.querySelector("#menu");
+const contactButton = document.querySelector("#contact");
+
+navBar.addEventListener("click", (e) => {
+
+    //Clear the current page
+    content.textContent = "";
+
+    aboutButton.classList.remove("active");
+    menuButton.classList.remove("active");
+    contactButton.classList.remove("active");
+
+    if (e.target.id == "about"){
+
+        aboutButton.classList.add("active");
+        generateHome();
+
+
+    }else if (e.target.id == "menu"){
+
+        menuButton.classList.add("active");
+        generateMenu();
+
+    }else if (e.target.id == "contact"){
+
+        contactButton.classList.add("active");
+        generateContact();
+
+    }
+
+
+});
+
+//Initial Render
+aboutButton.classList.add("active");
+generateHome();
